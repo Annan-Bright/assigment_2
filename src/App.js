1271908da5fe1e60655c652cf20e738d3eab9c37
Bright Annan <brightannan5@gmail.com>
2021-03-11 11:30:08 +0000

    added react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,83 +1,82 @@
-import React from "react";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+import Users from "./components/Users";
+import AddUserForm from "./components/AddUserForm";
 import "./App.css";
-import UserItem from "./components/UserItem";
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "",
-      email: "",
-      password: "",
-      users: [],
+      users: [
+        {
+          name: "Bright Annan",
+          email: "brightannan5@gmail.com",
+          gen: 16,
+          id: "fglgfllfg545874",
+        },
+
+        {
+          name: "Ronald Amoo",
+          email: "bustorme@gmail.com",
+          gen: 17,
+          id: "rgdfkffko5468",
+        },
+
+        {
+          name: "Seyram Hormeku",
+          email: "hormekuseyram@gmail.com",
+          gen: 15,
+          id: "rgplopkf3554",
+        },
+      ],
     };
   }
-  handleNameChange = (e) => {
-    this.setState({ name: e.target.value });
+  addNewUser = (user) => {
+    user.id = Math.random().toString();
+    this.setState({
+      users: [...this.state.users, user],
+    });
   };
-  handleEmailChange = (e) => {
-    this.setState({ email: e.target.value });
+  deleteUser = (id) => {
+    let undeletedUsers = this.state.users.filter((user) => user.id !== id);
+    console.log(undeletedUsers);
+    this.setState({
+      users: undeletedUsers,
+    });
   };
-  handlePasswordChange = (e) => {
-    this.setState({ password: e.target.value });
+  editUser = (id, updatedUser) => {
+    this.setState({
+      users: this.state.users.map((user) =>
+        user.id === id ? updatedUser : user
+      ),
+    });
   };
-  handleSubmit = (e) => {
-    e.preventDefault();
 
-    let newUser = {
-      name: this.state.name,
-      email: this.state.email,
-      password: this.state.password,
-    };
-    this.setState({ users: [...this.state.users, newUser] });
-
-    this.setState({ name: "", email: "", password: "" });
-  };
   render() {
     return (
-      <div className="App">
-        {/* Form to add new user */}
-        <div>
-          <form onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              placeholder="Name"
-              value={this.state.name}
-              onChange={this.handleNameChange}
-            />
-            <br />
-            <input
-              type="email"
-              placeholder="Email"
-              value={this.state.email}
-              onChange={this.handleEmailChange}
-            />
-            <br />
-            <input
-              type="password"
-              placeholder="Password"
-              value={this.state.password}
-              onChange={this.handlePasswordChange}
-            />
-            <br />
-            <input type="submit" />
-          </form>
-        </div>
-
-        {/* List of users */}
-        <div>
-          {this.state.users.map((user, index) => (
-            <UserItem
-              key={index}
-              name={user.name}
-              email={user.email}
-              password={user.password}
-            />
-          ))}
-        </div>
-      </div>
+      <>
+        <Container fluid style={{ marginTop: "2rem" }}>
+          <Row>
+            <Col md="4">
+              <h4>Add Users</h4>
+              <br />
+              <AddUserForm addUser={this.addNewUser} />
+            </Col>
+            <Col>
+              <h4>All Codetrain Users</h4>
+              <br />
+              <Users
+                userData={this.state.users}
+                deleteUser={this.deleteUser}
+                editUser={this.editUser}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </>
     );
   }
 }
-
 export default App;
